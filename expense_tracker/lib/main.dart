c1291a4998dc15b90147664b116144942a695819
SuvrangshuBarua <suvrangshu.turno@gmail.com>
2019-12-04 21:19:40 +0600

    Eighth commit

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,5 +1,10 @@
-import './widgets/user_transactions.dart';
+import 'package:expense_tracker/widgets/transaction_list.dart';
+
+import './widgets/new_transactions.dart';
+
+import './widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
+import './models/transaction.dart';
 
 void main() => runApp(MyApp());
 const PrimaryColor = const Color(0xff162229);
@@ -12,15 +17,61 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: PrimaryColor,
+        fontFamily: 'Quicksand',
       ),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.35,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Black Friday Shopping',
+      amount: 86.56,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      backgroundColor: Color(0xffe7eaef),
+      context: ctx,
+      builder: (bCtx) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +82,7 @@ class MyHomePage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () => _startAddNewTransaction(context),
           )
         ],
       ),
@@ -55,7 +106,7 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            UserTransactions()
+            TransactionList(_userTransactions),
           ],
         ),
       ),
@@ -63,7 +114,7 @@ class MyHomePage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: PrimaryColor,
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
--- expense_tracker/lib/widgets/new_transactions.dart
+++ expense_tracker/lib/widgets/new_transactions.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 
 const PrimaryColor = const Color(0xff162229);
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
+
   void submitData() {
     final enteredTitle = titleController.text;
     final enteredAmount = double.parse(amountController.text);
@@ -15,47 +23,44 @@ class NewTransaction extends StatelessWidget {
     if (enteredAmount <= 0 || enteredTitle.isEmpty) {
       return;
     }
-    addTx(
+    widget.addTx(
       enteredTitle,
       enteredAmount,
     );
+    Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Card(
-        color: Color(0xffe7eaef),
-        elevation: 5,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: titleController,
-              onSubmitted: (_) => submitData(),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => submitData(),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FlatButton(
-                color: PrimaryColor,
-                child: Text(
-                  'Add Transaction',
-                  style: TextStyle(color: Colors.white),
-                ),
-                textColor: PrimaryColor,
-                onPressed: submitData,
+    return Card(
+      color: Color(0xffe7eaef),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Title'),
+            controller: titleController,
+            onSubmitted: (_) => submitData(),
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Amount'),
+            controller: amountController,
+            keyboardType: TextInputType.number,
+            onSubmitted: (_) => submitData(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FlatButton(
+              color: PrimaryColor,
+              child: Text(
+                'Add Transaction',
+                style: TextStyle(color: Colors.white),
               ),
-            )
-          ],
-        ),
+              textColor: PrimaryColor,
+              onPressed: submitData,
+            ),
+          )
+        ],
       ),
     );
   }
